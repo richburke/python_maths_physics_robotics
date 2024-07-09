0b1cf77ce4aded41fc1d5abaca69d83b9164ee96
Rich Burke <rich.burke.dev@gmail.com>
2024-07-09 01:49:26 +0000

    Add period and amplitude to trigonometry graphing

--- exec.py
+++ exec.py
@@ -6,16 +6,17 @@ from maths.algebra.functions import is_function_even, is_function_odd
 from maths.geometry.defines import RightTriangleLeg, RightTriangleFields
 from maths.geometry.circle import radius_from_origin_and_point
 from maths.geometry.triangle import make_right_triangle, right_triangle_values_from_angle_leg, right_triangle_remaining_angle, euclidean_distance, adjacent_leg_from_right_triangle, hypotenuse_from_right_triangle, opposite_leg_from_right_triangle
-from maths.trigonometry.defines import PI, TWO_PI, PI_OVER_TWO, THREE_PI_OVER_TWO, QuadrantLocation
+from maths.trigonometry.defines import PI, TWO_PI, PI_OVER_TWO, THREE_PI_OVER_TWO, QuadrantLocation, TrigFunction
 from maths.trigonometry.angle import degrees_to_radians, radians_to_degrees, to_principal_interval, to_positive_angle, to_negative_angle, number_of_full_rotations, angle_from_xy, angle_after_rotations
 from maths.trigonometry.identities import values_from, secant_from_values, cotangent_from_values
-from maths.trigonometry.unit_circle import coterminal_angles_within_interval
+from maths.trigonometry.unit_circle import coterminal_angles_within_interval, display_common_unit_circle_coordinates
 from maths.trigonometry.circle import sector_area, revolutions_to_radians
 from physics.velocity import linear_velocity, angular_velocity, angular_velocity_from_revolutions_per_minute, linear_velocity_from_angular_velocity, angular_velocity_from_linear_velocity, angular_velocity_from_revolutions_per_second
-from maths.trigonometry.graphing import plot_sine_wave
+from maths.trigonometry.graphing import plot_sine, plot_cosine, plot_sine_with_unscaled, plot_cosine_with_unscaled, plot_cosecant_with_unscaled, plot_cosecant, plot_secant, plot_secant_with_unscaled, period, amplitude
 
 
 from matplotlib import pyplot as plt
+import numpy as np
 
 def main(args=None):
     
@@ -31,8 +32,24 @@ def main(args=None):
     # print(.620 * PI)
     # print(revolutions_per_minute(x), " revolutions per minute")
 
-    # x = []
-    plot_sine_wave()
+    # theta = np.linspace(-PI, 4 * PI)
+    # plot_secant_with_unscaled(theta)(-2)
+
+    p = period(TrigFunction.SINE)(PI ** 2)
+    print(p, 2 / PI)
+    p = period(TrigFunction.TANGENT)(4/3)
+    print(p, 3 * PI / 4)
+    p = period(TrigFunction.SECANT)(1/3)
+    print(p, 6 * PI)
+    p = period(TrigFunction.COTANGENT)(4 * PI / 3)
+    print(p, 3 / 2)
+
+
+    # display_common_unit_circle_coordinates()
+    # plot_cosecant_with_unscaled(theta)()
+    # plot_sine_wave_with_unscaled(theta)(2, 1/2)
+    # plot_sine_wave_with_unscaled(theta)(1/2)
+    # plot_cosine_wave_with_unscaled(theta)(2, 2)
 
 
     # xs = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
--- maths/trigonometry/graphing.py
+++ maths/trigonometry/graphing.py
@@ -1,9 +1,21 @@
 import os
 import sys
 import math
+import numpy as np
 import matplotlib.pyplot as plt
+from maths.trigonometry.defines import PI, TWO_PI, TrigFunction
+from maths.trigonometry.identities import sin_csc
 
 sys.path.extend([os.getcwd()])
+
+RANGE_PRINCIPAL_INTERVAL = np.linspace(0, TWO_PI, 360)
+
+'''
+@todo
+If I get the time I'd like to improve these graphs. The first place to start
+would be labeling the X-axis. It would be nice to see those in radians.
+https://mikequentelsoftware.blogspot.com/2012/10/example-of-trigonometric-functions-in.html
+'''
 
 '''
 def plot_polar(fn, start, end, step):
@@ -13,25 +25,118 @@ def plot_polar(fn, start, end, step):
     plt.show()
 '''
 
-def y_sine(x, a, b, x_shift = 0, y_shift = 0):
-    return a * math.sin(b * (x - x_shift)) + y_shift
+def y_sine(x, vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+    return vertical_scalar * math.sin(horizontal_scalar * (x - x_shift)) + y_shift
 
-def y_cosine(x, a, b, x_shift = 0, y_shift = 0):
-    return a * math.cos(b * (x - x_shift)) + y_shift
+def y_cosine(x, vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+    return vertical_scalar * math.cos(horizontal_scalar * (x - x_shift)) + y_shift
 
-def y_tangent(x, a, b, x_shift = 0, y_shift = 0):
-    return a * math.tan(b * (x - x_shift)) + y_shift
+def y_tangent(x, vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+    return vertical_scalar * math.tan(horizontal_scalar * (x - x_shift)) + y_shift
 
-def y_cosecant(x, a, b, x_shift = 0, y_shift = 0):
-    return a * (1 / math.sin(b * (x - x_shift))) + y_shift
+def y_cosecant(x, vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+    csc = sin_csc( math.sin(horizontal_scalar * (x - x_shift)))
+    return vertical_scalar * csc + y_shift
 
-def y_secant(x, a, b, x_shift = 0, y_shift = 0):
-    return a * (1 / math.cos(b * (x - x_shift))) + y_shift
+def y_secant(x, vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+    return vertical_scalar * (1 / math.cos(horizontal_scalar * (x - x_shift))) + y_shift
 
-def y_cotangent(x, a, b, x_shift = 0, y_shift = 0):
-    return a * (1 / math.tan(b * (x - x_shift))) + y_shift
+def y_cotangent(x, vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+    return vertical_scalar * (1 / math.tan(horizontal_scalar * (x - x_shift))) + y_shift
 
-def plot_sine_wave(x = [-math.pi / 2, -math.pi/ 4, 0, math.pi / 4, math.pi / 2], a = 1, b = 1, x_shift = 0, y_shift = 0):
-    y = [y_sine(i, a, b, x_shift, y_shift) for i in x]
-    plt.plot(x, y)
-    plt.show()
+def amplitude_from_scalar(vertical_scalar = 1):
+    np.abs(vertical_scalar)
+
+def amplitude(minmax):
+    min, max = minmax
+    np.abs((max - min) / 2)
+
+def period(trig_function = TrigFunction.SINE):
+    def fn(horizontal_scalar = 1):
+        if trig_function == TrigFunction.SINE or \
+           trig_function == TrigFunction.COSINE or \
+           trig_function == TrigFunction.SECANT or \
+           trig_function == TrigFunction.COSECANT:
+            return TWO_PI / np.abs(horizontal_scalar)
+        return PI / np.abs(horizontal_scalar)
+    return fn
+
+def plot_sine(theta = RANGE_PRINCIPAL_INTERVAL):
+    def fn(vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+        y = [y_sine(i, vertical_scalar, horizontal_scalar, x_shift, y_shift) for i in theta]
+        plt.plot(theta, y)
+        plt.show()
+    return fn
+
+def plot_sine_with_unscaled(theta = RANGE_PRINCIPAL_INTERVAL):
+    def fn(vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+        y1 = [y_sine(i) for i in theta]
+        y2 = [y_sine(i, vertical_scalar, horizontal_scalar, x_shift, y_shift) for i in theta]
+        plt.plot(theta, y1, 'k--')
+        plt.plot(theta, y2, 'b')
+        plt.show()
+    return fn
+
+def plot_cosine(theta = RANGE_PRINCIPAL_INTERVAL):
+    def fn(vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+        y = [y_cosine(i, vertical_scalar, horizontal_scalar, x_shift, y_shift) for i in theta]
+        plt.plot(theta, y)
+        plt.show()
+    return fn
+
+def plot_cosine_with_unscaled(theta = RANGE_PRINCIPAL_INTERVAL, vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+    def fn(vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0):
+        y1 = [y_cosine(i) for i in theta]
+        y2 = [y_cosine(i, vertical_scalar, horizontal_scalar, x_shift, y_shift) for i in theta]
+        plt.plot(theta, y1, 'k--')
+        plt.plot(theta, y2, 'b')
+        plt.show()
+    return fn
+
+def plot_cosecant(theta = RANGE_PRINCIPAL_INTERVAL):
+    def fn(vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0, y_axis_min=-4, y_axis_max=4):
+        y = [y_cosecant(i, vertical_scalar, horizontal_scalar, x_shift, y_shift) for i in theta]
+        
+        plt.xlim(theta.min() * 1.1, theta.max() * 1.1)
+        plt.ylim(y_axis_min, y_axis_max)
+
+        plt.plot(theta, y)
+        plt.show()
+    return fn
+
+def plot_cosecant_with_unscaled(theta = RANGE_PRINCIPAL_INTERVAL):
+    def fn(vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0, y_axis_min=-4, y_axis_max=4):
+        y1 = [y_sine(i) for i in theta]
+        y2 = np.array([y_cosecant(i, vertical_scalar, horizontal_scalar, x_shift, y_shift) for i in theta])
+
+        plt.xlim(theta.min() * 1.1, theta.max() * 1.1)
+        plt.ylim(y_axis_min, y_axis_max)
+        
+        plt.plot(theta, y1, 'k--')
+        plt.plot(theta, y2, 'b')
+        plt.show()
+    return fn
+
+def plot_secant(theta = RANGE_PRINCIPAL_INTERVAL):
+    def fn(vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0, y_axis_min=-4, y_axis_max=4):
+        y = [y_secant(i, vertical_scalar, horizontal_scalar, x_shift, y_shift) for i in theta]
+        
+        plt.xlim(theta.min() * 1.1, theta.max() * 1.1)
+        plt.ylim(y_axis_min, y_axis_max)
+
+        plt.plot(theta, y)
+        plt.show()
+    return fn
+
+def plot_secant_with_unscaled(theta = RANGE_PRINCIPAL_INTERVAL):
+    def fn(vertical_scalar = 1, horizontal_scalar = 1, x_shift = 0, y_shift = 0, y_axis_min=-4, y_axis_max=4):
+        y1 = [y_cosine(i) for i in theta]
+        y2 = np.array([y_secant(i, vertical_scalar, horizontal_scalar, x_shift, y_shift) for i in theta])
+
+        plt.xlim(theta.min() * 1.1, theta.max() * 1.1)
+        plt.ylim(y_axis_min, y_axis_max)
+        
+        plt.plot(theta, y1, 'k--')
+        plt.plot(theta, y2, 'b')
+        plt.show()
+    return fn
